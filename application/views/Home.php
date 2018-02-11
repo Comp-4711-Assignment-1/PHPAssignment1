62126c5287db395b82f8f2bdfa86e4af209c362a
Krixium <bennywang75@hotmail.com>
2018-02-11 03:27:11 -0800

    Added default set layout and dropdown menu

--- application/views/Home.php
+++ application/views/Home.php
@@ -3,9 +3,50 @@
 		<div class="col-2 text-center">
 		</div>
 		<div class="col-8 text-center">
-			<h1>Main content</h1>
+			<h1 class="display-1">Home</h1>
 		</div>
 		<div class="col-2 text-center">
+		</div>
+	</div>
+
+	<div class="row border-top border-bottom">
+		<div class="col-12">
+			<table style="margin: auto;">
+				<tr>
+					<td><img src="assets/images/item1.png"></td>
+					<td><img src="assets/images/item1.png"></td>
+					<td><img src="assets/images/item1.png"></td>
+				</tr>
+				<tr>
+					<td><img src="assets/images/item1.png"></td>
+					<td><img src="assets/images/item1.png"></td>
+					<td><img src="assets/images/item1.png"></td>
+				</tr>
+				<tr>
+					<td><img src="assets/images/item1.png"></td>
+					<td><img src="assets/images/item1.png"></td>
+					<td><img src="assets/images/item1.png"></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12">
+			<div class="dropdown text-center">
+				<button class="btn btn-lg btn-primary dropdown-toggle" 
+						type="button" 
+						id="dropdownMenuButton" 
+						data-toggle="dropdown" 
+						aria-haspopup="true" 
+						aria-expanded="false">
+						Sets
+					</button>
+				<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<a class="dropdown-item" href="/set/1">Set 1</a>
+					<a class="dropdown-item" href="/set/2">Set 2</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
