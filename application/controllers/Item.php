6357a0c3eb2f0eb5f7812f83601cf18a3d785db0
Tim Bruecker <brooker_113@hotmail.com>
2018-04-07 18:40:09 -0700

    View done

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -16,7 +16,81 @@ class Item extends Application
 		$this->data['pagetitle'] = 'Edit ' . $category . " - " . $id;
 		$this->data['pagebody'] = 'ItemEdit';
 
+		$itemArray = array();
+
+		if($category == 'barrel')
+		{
+			$this->load->model('BarrelModel');
+			$item = $this->BarrelModel->get($id);
+		}
+		else if($category == 'body')
+		{
+			$this->load->model('BodyModel');
+			$item = $this->BodyModel->all();
+		}
+		else if($category == 'grip')
+		{
+			$this->load->model('GripModel');
+			$item = $this->GripModel->all();
+		}
+		else if($category == 'sight')
+		{
+			$this->load->model('SightModel');
+			$item = $this->SightModel->all();
+		}
+		else if($category == 'stock')
+		{
+			$this->load->model('StockModel');
+			$item = $this->StockModel->all();
+		}
+		
+		$this->data['id'] = $id;
+		$this->data['category'] = $category;
+		$this->data['name'] = $item->Name;
+		$this->data['desc'] = $item->Description;
+		$this->data['acc'] = $item->Accuracy;
+		$this->data['fr'] = $item->FireRate;
+		$this->data['dam'] = $item->Damage;
+		$this->data['img'] = '<img src="/' . strtolower($item->Filename) . '" class="img-fluid">';
 		$this->render();
 	}
-	
+
+	public function save($category, $id)
+	{
+		if($category == 'barrel')
+		{
+			$this->load->model('BarrelModel');
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules($this->SetModel->rules());
+		}
+		else if($category == 'body')
+		{
+			$this->load->model('BodyModel');
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules($this->SetModel->rules());
+		}
+		else if($category == 'grip')
+		{
+			$this->load->model('GripModel');
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules($this->SetModel->rules());
+		}
+		else if($category == 'sight')
+		{
+			$this->load->model('SightModel');
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules($this->SetModel->rules());
+		}
+		else if($category == 'stock')
+		{
+			$this->load->model('StockModel');
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules($this->SetModel->rules());
+		}
+
+		
+		
+	}
 }
+	
+
